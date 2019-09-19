6552b6cbbe6f0d812b8b83a08a47c8b794e9bfc8
Riya Dattani <riyaddattani@gmail.com>
2019-09-19 14:45:14 +0100

    delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 const usersData = [
     { id: 1, name: 'Riya', username: 'browncat' },
     { id: 2, name: 'Chris', username: 'coolcat' },
     { id: 3, name: 'Lisa', username: 'vegancat' },
-    { id: 3, name: 'Tamara', username: 'sportycat' }
+    { id: 4, name: 'Tamara', username: 'sportycat' }
 ]
+
 
 const App = () => {
     const [users, setUsers] = useState(usersData)
+
+    const addUser = user => {
+        user.id = users.length + 1
+        setUsers([...users, user])
+    }
+    const deleteUser = id => {
+        setUsers(users.filter(user => user.id !== id))
+    }
+
     return (
         <div className="container">
             <h1>Your Little Contact Book</h1>
             <div className="flex-row">
                 <div className="flex-large">
                     <h2>Add Contact</h2>
+                    <AddUserForm addUser={addUser} />
                 </div>
                 <div className="flex-large">
                     <h2>View All Contacts</h2>
-                    <UserTable users={users} />
+                    <UserTable users={users} deleteUser={deleteUser} />
                 </div>
             </div>
         </div>
